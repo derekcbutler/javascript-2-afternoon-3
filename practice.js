d0094a579aa98475554a49122de026b2347429da
Derek Butler <derekcbutler@gmail.com>
2019-09-11 21:49:17 -0600

    night2

--- practice.js
+++ practice.js
@@ -28,6 +28,10 @@
 */
 
 // Code Here 
+function first(array, callback){
+  callback(array[0]);
+}
+
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -48,6 +52,9 @@ first(names, function(firstName){
 */
 
 //Code Here
+function last(array, callback){
+  callback(array[array.length -1]);
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -66,6 +73,9 @@ last(names, function(lastName){
 */
 
 //Code Here
+function multiply(num1, num2, callback){
+  callback(num1 * num2);
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -85,6 +95,9 @@ multiply(4, 3, function(answer){
 */
 
 //Code Here 
+function contains(array, name, callback){
+  
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -106,6 +119,16 @@ contains(names, 'Colt', function(result){
 */
 
 //Code Here
+function uniq(array, callback){
+  for(let i = 0; i <= array.length; i++){
+    for(let j = array.length; j >= 0; j--){
+      if(array[i] === array[j] && i !== j){
+        array.splice(i, 1)
+      }
+    }
+  }callback(array)
+  return array;
+ }
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
@@ -123,6 +146,9 @@ uniq(names, function(uniqArr){
 */
 
 //Code Here 
+function each(names, callback){
+  
+}
 
 // Do not edit the code below.
 each(names, function(item, indice){
